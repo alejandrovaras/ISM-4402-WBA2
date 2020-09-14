30eb3193835fc85cae55ee1a93075bb78a7491a6
alejandrovaras <70179469+alejandrovaras@users.noreply.github.com>
2020-09-14 11:39:17 -0400

    Add files via upload

---
+++ Page 13.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+names = ['Nike','Adidas','New Balance','Puma',’Reebok’]
+grades = [176,59,47,38,99]
+PriceList = zip(names,prices)
+df = pd.DataFrame(data = PriceList, columns=['Names',’Prices’])
+
+
+# In[ ]:
+
+
+
+
